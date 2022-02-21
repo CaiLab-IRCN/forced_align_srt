36ff51820490ea91db1e4b0aa5a9ed8a9ab58d3b
ronald976 <526473+gb96@users.noreply.github.com>
2022-02-21 19:55:54 +0100

    update README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # forced_align_srt
+
+Fork of Penn Phonetics Lab Forced Aligner for use with movies. 
+Takes:
+- A subtitle file (.srt)
+- The movie audio (.wav)
+
+Outputs:
+- A .csv file with the onset and offset of each word in the movie.  
+
+Instructions:
+- Install p2fa
+- Set up a python 2.7 environment (or try this python 3 fork https://github.com/jaekookang/p2fa_py3)
+- Specify folder structure in force_align_subtitles.py
+- Run force_align_subtitles.py
+
+Dependencies:
+https://babel.ling.upenn.edu/phonetics/old_website_2015/p2fa/index.html (runs on python 2.7)
+
---
+++ force_align_subtitles.py
@@ -0,0 +1,84 @@
+import numpy as np
+import os
+import re
+from tqdm import tqdm
+import pandas as pd
+
+from IPython import embed
+
+# assumes SRT format: 
+# <subtitle number>
+# <start> --> <end> (hr:min:sec,ms)
+# <subtitle1>
+# <subtitle2> (optional)
+# \n
+
+
+remove_temp = True
+
+loc_py = "/home/ron/Downloads/forrest_stimulus/forced_alignment/p2fa/p2fa_1.003/p2fa/align2.py"
+loc_wav = "/home/ron/Downloads/forrest_stimulus/forrest_PAL/output4.wav"
+loc_sub = "/home/ron/Downloads/forrest_stimulus/forrest_PAL/release-English_utf.srt"
+loc_out = "/home/ron/Downloads/forrest_stimulus/forced_alignment/out/"
+
+with open(loc_sub, 'r') as file:
+    subs_raw=file.readlines()
+
+subs_raw = [i.replace('\n', '') for i in subs_raw]
+
+headers_idx = []
+starts = []
+ends = []
+# words = []
+
+print("pre-processing: extracting individual lines from .SRT")
+count = 0
+for c,i in enumerate(subs_raw):
+	if len(re.findall('^[1-9]', i)) > 0:
+		headers_idx.append(count)
+		count+=1 
+
+		times = subs_raw[c+1]
+		tstart = re.findall('^(.*?) --> ', times)[0].replace(":",",").split(",")
+		tstart = [int(val) for val in tstart]
+		starts.append(tstart[0]*3600 + tstart[1]*60 + tstart[2] + tstart[3]/1000) 
+		tend = re.findall(' --> (.*)', times)[0].replace(":",",").split(",")
+		tend = [int(val) for val in tend]
+		ends.append(tend[0]*3600 + tend[1]*60 + tend[2] + tend[3]/1000) 
+		
+		word = (subs_raw[c+2] + " " + subs_raw[c+3]+"\n").replace(" ","\n").replace("\n\n","\n").replace("\n\'","\n\\\'")
+		if word[0] == "'":
+			word = "\\"+word
+
+		with open(loc_out+"__temp%s.txt"%(headers_idx[-1]), "w") as text_file:
+			text_file.write(word)
+
+
+# print("performing forced-alignment")
+# for i in tqdm(headers_idx):
+# 	execute = "python %s -s %.2f -e %.2f %s %s__temp%d.txt %s__temp%d.TextGrid"%(loc_py, starts[i], ends[i], loc_wav, loc_out, i, loc_out, i)
+# 	os.system(execute)
+
+
+print("aggregating timings")
+all_starts = []; all_ends = []; all_words = []
+for i in headers_idx:
+	with open("%s__temp%d.TextGrid"%(loc_out, i), 'r') as file:
+		tmp=file.read().splitlines()
+	start_idx = np.flatnonzero(np.asarray(tmp) == '"word"')[0]
+	tmp = tmp[start_idx+4:]
+	starts 	= tmp[::3]
+	ends 	= tmp[1::3]
+	words 	= tmp[2::3]
+	for c,w in enumerate(words):
+		if not w == '"sp"':
+			all_starts.append(starts[c])
+			all_ends.append(ends[c])
+			all_words.append(w[1:-1])
+
+embed()
+
+savedat = pd.DataFrame()
+savedat["start"] = all_starts
+savedat["end"] = all_ends
+savedat["word"] = all_words
